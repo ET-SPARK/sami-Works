6c2da653287450533d4611409f9390cdb64d4865
ET-SAPRK <samuelwoyesso2016@gmail.com>
2024-06-21 10:47:08 +0300

    edited

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Landing from "@/components/Landing/page";
 import MenuBar from "@/components/MenuBar/page";
-
+import Projects from "@/components/Projects/page";
 
 export default function Home() {
   return (
     <>
       <MenuBar />
       <Landing />
+      <Projects />
     </>
   );
 }
---
+++ src/components/Projects/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <div className="mt-10">
+      <div className="text-center font-semibold uppercase text-4xl">
+        Projects
+      </div>
+      ;
+    </div>
+  );
+}
+
+export default Projects;
