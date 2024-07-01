4f0243c1b92d4a5f6bbb5dce0af3b5d43eab526a
ET-SAPRK <samuelwoyesso2016@gmail.com>
2024-07-01 11:46:51 +0300

    link added to the project

--- src/components/Projects/page.tsx
+++ src/components/Projects/page.tsx
@@ -14,6 +14,7 @@ import ApkIcon from "../../../public/apk.png";
 import { Button } from "../ui/button";
 import { FileCode, Github, Globe } from "lucide-react";
 import { Protest_Revolution } from "next/font/google";
+import Link from "next/link";
 
 const julius = Protest_Revolution({
   subsets: ["latin"],
@@ -26,7 +27,13 @@ function Projects() {
       image: SImage,
       title: "LMS",
       badge: ["vue", "shadcn-vue", "nuxt", "supabase"],
-      type: ["site", "apk", "source"],
+      type: [
+        {
+          site: "https://desiny-plus.web.app/",
+          apk: "",
+          source: "https://github.com/ET-SPARK/Walt-Disny-Clone",
+        },
+      ],
       description:
         "A Learning Management System (LMS) is a software application designed to manage, deliver, and track educational courses or training programs.",
     },
@@ -34,7 +41,13 @@ function Projects() {
       image: SImage,
       title: "Weather Forecast App",
       badge: ["angular", "typescript"],
-      type: ["site", "source"],
+      type: [
+        {
+          site: "https://weather-forecast-app.com",
+          apk: "",
+          source: "https://github.com/username/weather-forecast-app",
+        },
+      ],
       description:
         "A Weather Forecast App that provides users with up-to-date weather information. It includes features like real-time weather updates, forecasts, and weather alerts to help users plan their activities accordingly.",
     },
@@ -42,7 +55,13 @@ function Projects() {
       image: SImage,
       title: "Project Management Tool",
       badge: ["react", "redux", "node", "express"],
-      type: ["site", "source"],
+      type: [
+        {
+          site: "https://project-management-tool.com",
+          apk: "",
+          source: "https://github.com/username/project-management-tool",
+        },
+      ],
       description:
         "A Project Management Tool designed to help teams plan, track, and manage their projects effectively. It provides features like task assignments, progress tracking, and collaboration tools, ensuring projects are completed on time and within budget.",
     },
@@ -50,7 +69,13 @@ function Projects() {
       image: SImage,
       title: "E-commerce Platform",
       badge: ["next.js", "tailwindcss", "mongodb"],
-      type: ["site", "source"],
+      type: [
+        {
+          site: "https://ecommerce-platform.com",
+          apk: "",
+          source: "https://github.com/username/ecommerce-platform",
+        },
+      ],
       description:
         "An E-commerce Platform that allows businesses to set up and manage online stores. It offers features like product listings, shopping cart, payment processing, and order management to streamline the online shopping experience.",
     },
@@ -58,7 +83,13 @@ function Projects() {
       image: SImage,
       title: "Social Media App",
       badge: ["flutter", "firebase"],
-      type: ["apk", "source"],
+      type: [
+        {
+          site: "",
+          apk: "https://apk-link.com/social-media-app",
+          source: "https://github.com/username/social-media-app",
+        },
+      ],
       description:
         "A Social Media App developed to connect people and facilitate social interactions. It includes features like user profiles, posts, comments, and messaging, making it easy to share and communicate with others.",
     },
@@ -66,7 +97,13 @@ function Projects() {
       image: SImage,
       title: "Fitness Tracker",
       badge: ["react native", "expo", "firebase"],
-      type: ["apk", "source"],
+      type: [
+        {
+          site: "",
+          apk: "https://apk-link.com/fitness-tracker",
+          source: "https://github.com/username/fitness-tracker",
+        },
+      ],
       description:
         "A Fitness Tracker App designed to help users monitor their fitness activities, set goals, and track progress. It includes features like workout logging, goal setting, and activity tracking to support a healthy lifestyle.",
     },
@@ -74,7 +111,13 @@ function Projects() {
       image: SImage,
       title: "Online Booking System",
       badge: ["django", "postgresql"],
-      type: ["site", "source"],
+      type: [
+        {
+          site: "https://online-booking-system.com",
+          apk: "",
+          source: "https://github.com/username/online-booking-system",
+        },
+      ],
       description:
         "An Online Booking System that enables businesses to manage bookings and reservations. It offers features like calendar integration, automated reminders, and payment processing to streamline the booking process.",
     },
@@ -83,7 +126,7 @@ function Projects() {
   return (
     <div className="mt-10">
       <div className="text-center font-semibold uppercase text-4xl py-10 drop-shadow-white">
-        <p className={julius.className}> Projects</p>
+        <p className={julius.className}>Projects</p>
       </div>
       <div className="grid grid-cols-3 px-4 gap-4 max-[640px]:grid-cols-1 max-[1024px]:grid-cols-2">
         {myProject.map((project, index) => (
@@ -109,23 +152,44 @@ function Projects() {
               </CardDescription>
             </CardContent>
             <CardFooter className="border-t pt-4">
-              {project.type.includes("site") && (
-                <Button>
-                  <Globe className="h-[1.2rem] w-[1.2rem] mr-2" />
-                  Site
-                </Button>
+              {project.type[0].site && (
+                <Link
+                  href={project.type[0].site}
+                  passHref
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button>
+                    <Globe className="h-[1.2rem] w-[1.2rem] mr-2" />
+                    Site
+                  </Button>
+                </Link>
               )}
-              {project.type.includes("apk") && (
-                <Button className="ml-2">
-                  <FileCode className="h-[1.2rem] w-[1.2rem] mr-2" />
-                  APK File
-                </Button>
+              {project.type[0].apk && (
+                <Link
+                  href={project.type[0].apk}
+                  passHref
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button className="ml-2">
+                    <FileCode className="h-[1.2rem] w-[1.2rem] mr-2" />
+                    APK File
+                  </Button>
+                </Link>
               )}
-              {project.type.includes("source") && (
-                <Button className="ml-2">
-                  <Github className="h-[1.2rem] w-[1.2rem] mr-2" />
-                  Source
-                </Button>
+              {project.type[0].source && (
+                <Link
+                  href={project.type[0].source}
+                  passHref
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button className="ml-2">
+                    <Github className="h-[1.2rem] w-[1.2rem] mr-2" />
+                    Source
+                  </Button>
+                </Link>
               )}
             </CardFooter>
           </Card>
